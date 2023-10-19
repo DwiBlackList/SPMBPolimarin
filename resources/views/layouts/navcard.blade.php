1df616befa4cb122fb58d5e2d4de93ec3242118d
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-19 13:08:11 +0700

    pemindahan navigasi kartu

---
+++ resources/views/layouts/navcard.blade.php
@@ -0,0 +1,101 @@
+<div class="col-md-4 col-12">
+    <div class="card">
+        <div class="card-body">
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Home
+                </div>
+            </a>
+            <hr>
+            <a href="https://akademik.polimarin.ac.id/pmb/" target="_blank">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Portal SPMB
+                </div>
+            </a>
+            <hr>
+            <a href="{{ route('jadwal-pendaftaran') }}">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Jadwal Pendaftaran
+                </div>
+            </a>
+            <hr>
+            <a href="{{ route('persyaratan') }}">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Persyaratan
+                </div>
+            </a>
+            <hr>
+            <a href="{{ route('alur-pendaftaran') }}">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Alur Pendaftaran
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Seleksi Jalur Prestasi
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Seleksi Jalur Tes
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Seleksi Jalur Mandiri
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Tes Kekhususan
+                </div>
+            </a>
+            <hr>
+            <a href="{{ route('biaya') }}">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Biaya
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Kontak
+                </div>
+            </a>
+            <hr>
+            <a href="/">
+                <div class="d-flex align-content-center">
+                    <span class="material-symbols-outlined">
+                        arrow_forward_ios
+                    </span> Download
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
