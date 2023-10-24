45bd81adab8858ef0a568d61d342fa6a1206403f
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-24 10:56:59 +0700

    program alih jenjang remake

--- resources/views/program-alih-jenjang.blade.php
+++ resources/views/program-alih-jenjang.blade.php
@@ -2,48 +2,41 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>
-                Program Alih Jenjang (Transer SKS)
-            </h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>Program Alih Jenjang (SKS Transfer)</h1>
+    <p>Dalam rangka penguatan pendidikan vokasi dan meningkatkan peran Pendidikan Tinggi Vokasi untuk meningkatkan daya saing nasional internasional serta mewujudkan kemitraan yang kuat dengan dunia industri (IDUKA) dalam sinkronisasi link and match melalui pengembangan kurikulum, magang Industri, penerapan belajar project based learning dan case study untuk menghasilkan lulusan yang relevan dengan pasar kerja dan langsung terserap di dunia kerja. Politeknik Maritim Negeri Indonesia (POLIMARIN) memberikan kesempatan bagi para lulusan diploma tiga (D3) Program Studi Nautika untuk melanjutkan pendidikan ke jenjang yang lebih tinggi untuk meningkatkan mutu lulusan Sarjana Terapan dan melaksanakan (Rencana Strategis) Renstra Kementerian Pendidikan dan Kebudayaan dalam upaya meningkatkan sumber daya manusia Pendidikan Tinggi Vokasi terampil dan unggul serta relevan dengan IDUKA. Politeknik Maritim Negeri Indonesia (Polimarin) di Semarang membuka pendaftaran baru Program Alih Jenjang Program Sarjana Terapan Program Studi Nautika. Program Alih Jenjang ini para mahasiswa akan menempuh perkuliahan selama 2 semester.</p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
         <div class="row justify-content-center">
             <!-- Konten Kiri -->
-            <div class="col-md-8 col-12">
-                <p>
-                    Dalam rangka penguatan pendidikan vokasi dan meningkatkan peran Pendidikan Tinggi Vokasi untuk meningkatkan daya saing nasional internasional serta mewujudkan kemitraan yang kuat dengan dunia industri (IDUKA) dalam sinkronisasi link and match melalui pengembangan kurikulum, magang Industri, penerapan belajar project based learning dan case study untuk menghasilkan lulusan yang relevan dengan pasar kerja dan langsung terserap di dunia kerja. Politeknik Maritim Negeri Indonesia (POLIMARIN) memberikan kesempatan bagi para lulusan diploma tiga (D3) Program Studi Nautika untuk melanjutkan pendidikan ke jenjang yang lebih tinggi untuk meningkatkan mutu lulusan Sarjana Terapan dan melaksanakan (Rencana Strategis) Renstra Kementerian Pendidikan dan Kebudayaan dalam upaya meningkatkan sumber daya manusia Pendidikan Tinggi Vokasi terampil dan unggul serta relevan dengan IDUKA. Politeknik Maritim Negeri Indonesia (Polimarin) di Semarang membuka pendaftaran baru Program Alih Jenjang Program Sarjana Terapan Program Studi Nautika. Program Alih Jenjang ini para mahasiswa akan menempuh perkuliahan selama 2 semester.
-                </p>
-
-                <p class="text-bold">Biaya :</p>
-
+            <div class="col-12">
+                <p class="text-bold">Program Studi : D4 Nautika</p>
+                <p class="text-bold">“Jenjang D3 Nautika ke Jenjang Sarjana Terapan (D4) Nautika</p>
+                <p class="text-bold">Persyaratan</p>
+                <p>Pendaftar adalah lulusan Politeknik/Akademi/Sekolah Tinggi, Pelayaran </p>
+                <p>Membayar Biaya Pendaftaran.</p>
+                <p><strong>Mengumpulkan Berkas Pendaftaran:</strong></p>
+                <ul>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                    <li>Fotokopi Ijazah terakhir yang telah dilegalisir </li>
+                </ul>
+                <p><strong>Biaya Pendaftaran:</strong></p>
                 <ol>
                     <li>Biaya Pendaftaran Rp. 300.000,00 </li>
                     <li>Biaya Pendaftaran Rp. 300.000,00 </li>
+                    <li>Biaya Pendaftaran Rp. 300.000,00 </li>
+                    <li>Pembayaran Biaya Pendaftaran melalui Rek 0727272719 BPN 026 Polimarin Semarang Bank BNI 46 Cab Undip Pleburan. dan Konfirmasi Bp. Purwanto via WA: @087738601002</li>
                 </ol>
-
-                <p class="text-bold">Jadwal</p>
-
-                <ul>
-                    <li>Pendaftaran : 3 April s/d 31 Juli 2023</li>
-                    <li>Pendaftaran : 3 April s/d 31 Juli 2023</li>
-                </ul>
-
-                <p class="text-dangger">Sebelum melakukan pendaftaran, peserta di wajibkan untuk mengisi template formulir yang di sediakan pada link di bawah ini:</p>
-
-                <a href="#" class="p-5 btn btn-warning text-white">Daftar</a>
-
+                <div class="text-center">
+                    <a class="btn btn-warning" href="#" target="_blank" rel="noopener noreferrer">Daftar</a>
+                </div>
             </div>
-
-            <!-- Kartu Navigasi kanan -->
-            @include('layouts.navcard')
-
         </div>
     </div>
 </div>
