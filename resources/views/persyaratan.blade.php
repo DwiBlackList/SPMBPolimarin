4ea0334c976e02190e38182cd345bb07810772d7
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-24 10:04:07 +0700

    Persyaratan remake

--- resources/views/persyaratan.blade.php
+++ resources/views/persyaratan.blade.php
@@ -2,41 +2,47 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>Jadwal Pendaftaran</h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>PERSYARATAN</h1>
+    <p>Untuk Calon Mahasiswa Baru</p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
-        <div class="row justify-content-center">
-            <!-- Kartu Navigasi kiri -->
-            @include('layouts.navcard')
-            <!-- Konten Kanan -->
-            <div class="col-md-8 col-12">
-                <div class="table-responsive">
-                    <table class="table-bordered" width="100%">
-                        <tr>
-                            <td><h3 class="text-bold">PERSYARATAN UMUM PENDAFTARAN UNTUK SEMUA JALUR:</h3></td>
-                        </tr>
-                        <tr>
-                            <td>
-
-                                <ol>
-                                    <li>Warga Negara Indonesia (WNI)</li>
-                                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
-                                </ol>
-                                <br>
-                                asd
-
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-
+        <div class="row">
+            <div class="col-12">
+                <p class="h3 text-bold text-center">PERSYARATAN UMUM PENDAFTARAN UNTUK SEMUA JALUR:</p>
+                <ol>
+                    <li>Warga Negara Indonesia (WNI)</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Menyerahkan kelengkapan pendaftaran:</li>
+                    <ul>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                    </ul>
+                </ol>
+                <p class="h3 text-bold text-center">PERSYARATAN Asal Sekolah:</p>
+                <ol>
+                    <li>Warga Negara Indonesia (WNI)</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Usia maksimal 22 tahun per 1 Agustus 2023</li>
+                    <li>Menyerahkan kelengkapan pendaftaran:</li>
+                    <ul>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                        <li>Bukti lunas pendaftaran;</li>
+                    </ul>
+                </ol>
             </div>
         </div>
     </div>
