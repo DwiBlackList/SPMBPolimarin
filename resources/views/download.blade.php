c57691a2e10ab4213aedf598aa91fdf8790d3ffc
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-24 14:40:13 +0700

    Download page

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -2,28 +2,73 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>
-                Download
-            </h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>Download Page</h1>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
         <div class="row justify-content-center">
-
-            <!-- Konten Kanan -->
-            <div class="col-md-8 col-12">
-                <h1>test</h1>
+            <div class="col-12 col-md-6 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <span class="material-symbols-outlined">
+                            download
+                        </span>
+                        <p class="h6">PENGUMUMAN SELEKSI PROGRAM MAGISTER TERAPAN (GEL 2)</p>
+                        <p>Pengumuman hasil seleksi Program Magister Terapan POLINES 2023 (Gelombang 2)</p>
+                    </div>
+                </div>
             </div>
 
+            <div class="col-12 col-md-6 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <span class="material-symbols-outlined">
+                            download
+                        </span>
+                        <p class="h6">PENGUMUMAN SELEKSI PROGRAM MAGISTER TERAPAN (GEL 2)</p>
+                        <p>Pengumuman hasil seleksi Program Magister Terapan POLINES 2023 (Gelombang 2)</p>
+                    </div>
+                </div>
+            </div>
 
-            <!-- Kartu Navigasi kiri -->
-            @include('layouts.navcard')
+            <div class="col-12 col-md-6 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <span class="material-symbols-outlined">
+                            download
+                        </span>
+                        <p class="h6">PENGUMUMAN SELEKSI PROGRAM MAGISTER TERAPAN (GEL 2)</p>
+                        <p>Pengumuman hasil seleksi Program Magister Terapan POLINES 2023 (Gelombang 2)</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <span class="material-symbols-outlined">
+                            download
+                        </span>
+                        <p class="h6">PENGUMUMAN SELEKSI PROGRAM MAGISTER TERAPAN (GEL 2)</p>
+                        <p>Pengumuman hasil seleksi Program Magister Terapan POLINES 2023 (Gelombang 2)</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <span class="material-symbols-outlined">
+                            download
+                        </span>
+                        <p class="h6">PENGUMUMAN SELEKSI PROGRAM MAGISTER TERAPAN (GEL 2)</p>
+                        <p>Pengumuman hasil seleksi Program Magister Terapan POLINES 2023 (Gelombang 2)</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
