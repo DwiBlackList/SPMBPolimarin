9d65d853ec307f57a97a3597986c5e737407ae75
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-24 10:24:28 +0700

    biaya remake

--- resources/views/biaya.blade.php
+++ resources/views/biaya.blade.php
@@ -2,67 +2,136 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>Biaya</h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>Biaya</h1>
+    <p>“Pembayaran Biaya Pendaftaran dan Tes Kekhususan melalui Virtual Account hanya bisa dilakukan melalui BNI Channel yaitu ATM BNI, Mobile Banking BNI, SMS Banking BNI, Teller Bank BNI, Internet Banking BNI, dan Agen 46 BNI.”</p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
-        <div class="row justify-content-center">
-            <!-- Kartu Navigasi kiri -->
-            @include('layouts.navcard')
-            <!-- Konten Kanan -->
-            <div class="col-md-8 col-12">             
+        <div class="row">
+            <div class="col-md-6 col-12">
                 <div class="table-responsive">
-                    <table class="table-bordered" width="100%">
+                    <table class="table table-striped ">
                         <tr>
-                            <td colspan="4"><h4 class="text-bold">Biaya</h4></td>
+                            <td colspan="2"><strong>Seleksi Jalur Prestasi (SJP)</strong></td>
                         </tr>
                         <tr>
-                            <td>1.</td>
-                            <td class="text-bold">Jalur Prestasi</td>
-                            <td></td>
-                            <td class="text-bold">Biaya</td>
+                            <td>Kegiatan</td>
+                            <td>Waktu Pelaksanaan</td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>Biaya Pendaftaran</td>
-                            <td>:</td>
-                            <td>Gratis</td>
+                            <td>Pendaftaran</td>
+                            <td>1 Februari – 28 Februari 2023</td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>Biaya Tes</td>
-                            <td>:</td>
-                            <td>Rp.300.000</td>
+                            <td>Seleksi Administrasi</td>
+                            <td>2 Februari – 1 Maret 2023</td>
                         </tr>
                         <tr>
-                            <td>1.</td>
-                            <td class="text-bold">Jalur Prestasi</td>
-                            <td></td>
-                            <td class="text-bold">Biaya</td>
+                            <td>Seleksi Prestasi</td>
+                            <td>1 Maret 2023</td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>Biaya Pendaftaran</td>
-                            <td>:</td>
-                            <td>Gratis</td>
+                            <td>Tes Kesehatan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
                         </tr>
                         <tr>
-                            <td></td>
-                            <td>Biaya Tes</td>
-                            <td>:</td>
-                            <td>Rp.300.000</td>
+                            <td>Wawancara</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Psikotes</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Kesamaptaan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
                         </tr>
                     </table>
                 </div>
-                <br>
-                <h1 class="text-bold">TATA CARA PEMBAYARAN</h1>
-                <p>"“Pembayaran <strong> Biaya Pendaftaran dan Tes Kekhususan </strong> melalui Virtual Account hanya bisa dilakukan melalui BNI Channel yaitu ATM BNI, Mobile Banking BNI, SMS Banking BNI, Teller Bank BNI, Internet Banking BNI, dan Agen 46 BNI.”</p>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="table-responsive">
+                    <table class="table table-striped ">
+                        <tr>
+                            <td colspan="2"><strong>Seleksi Jalur Prestasi (SJP)</strong></td>
+                        </tr>
+                        <tr>
+                            <td>Kegiatan</td>
+                            <td>Waktu Pelaksanaan</td>
+                        </tr>
+                        <tr>
+                            <td>Pendaftaran</td>
+                            <td>1 Februari – 28 Februari 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Seleksi Administrasi</td>
+                            <td>2 Februari – 1 Maret 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Seleksi Prestasi</td>
+                            <td>1 Maret 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Kesehatan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Wawancara</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Psikotes</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Kesamaptaan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="table-responsive">
+                    <table class="table table-striped ">
+                        <tr>
+                            <td colspan="2"><strong>Seleksi Jalur Prestasi (SJP)</strong></td>
+                        </tr>
+                        <tr>
+                            <td>Kegiatan</td>
+                            <td>Waktu Pelaksanaan</td>
+                        </tr>
+                        <tr>
+                            <td>Pendaftaran</td>
+                            <td>1 Februari – 28 Februari 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Seleksi Administrasi</td>
+                            <td>2 Februari – 1 Maret 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Seleksi Prestasi</td>
+                            <td>1 Maret 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Kesehatan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Wawancara</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Psikotes</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                        <tr>
+                            <td>Tes Kesamaptaan</td>
+                            <td>1 Februari – 28 Februari 2023 (Portofolio)</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
