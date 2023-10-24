86378be7ae9be82a4ab01acdafdaca68c506c70d
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-24 09:21:10 +0700

    program studi remake

--- resources/views/program-studi.blade.php
+++ resources/views/program-studi.blade.php
@@ -2,52 +2,41 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>Program Studi</h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+  <h1>Program Studi</h1>
+  <p>Daftar Program Studi Lengkap <a class="text-white text-bold" href="http://polimarin.ac.id" target="_blank" rel="noopener noreferrer">http://polimarin.ac.id</a></p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
-        <div class="row justify-content-center">
-            <!-- Konten Kiri -->
-            <div class="col-md-8 col-12">
-                <h1 class="text-bold">Jurusan Nautika</h1>
-                <div class="table-responsive">
-
-                    <table class="table-bordered" width="100%">
-                        <tr>
-                            <td>Program Studi</td>
-                            <td>Jenjang Pendidikan</td>
-                            <td>Akreditasi</td>
-                        </tr>
-                        <tr>
-                            <td>Nautika</td>
-                            <td>D4</td>
-                            <td>Baik</td>
-                        </tr>
-                        <tr>
-                            <td>Nautika</td>
-                            <td>D3</td>
-                            <td>B</td>
-                        </tr>
-                    </table>
-                </div>
-
-                <br>
-
-                <p class="text-bold">Profil bla bla di <a href="http://polimarin.ac.id" target="_blank" class="text-primary">http://polimarin.ac.id</a></p>
-
-
-            </div>
-
-            <!-- Kartu Navigasi kanan -->
-            @include('layouts.navcard')
-
+        <p class="h1 text-bold">Jurusan Nautika</p>
+        <hr width="15%" style="border-width: 5px;">
+        <div class="bg-primary rounded-4 p-4 m-3 text-white">
+            <p class="h3 text-bold">Nautika (D4) <strong class="h6">Baik</strong></p>
         </div>
+        <div class="bg-primary rounded-4 p-4 m-3 text-white">
+            <p class="h3 text-bold">Nautika (D3) <strong class="h6">B</strong></p>
+        </div>
+    </div>
+    <div class="container">
+        <p class="h1 text-bold">JURUSAN TEKNIKA</p>
+        <hr width="15%" style="border-width: 5px;">
+        <div class="bg-primary rounded-4 p-4 m-3 text-white">
+            <p class="h3 text-bold">TEKNOLOGI REKAYASA PERMESINAN KAPAL (D4) <strong class="h6">Baik</strong></p>
+        </div>
+        <div class="bg-primary rounded-4 p-4 m-3 text-white">
+            <p class="h3 text-bold">TEKNIKA (D3) <strong class="h6">B</strong></p>
+        </div>
+    </div>
+    <div class="container">
+        <p class="h1 text-bold">JURUSAN TEKNIKA</p>
+        <hr width="15%" style="border-width: 5px;">
+        <div class="bg-primary rounded-4 p-4 m-3 text-white">
+            <p class="h3 text-bold">TRANSPORTASI LAUT (D4) <strong class="h6">B</strong></p>
+        </div>
+    </div>
+    <div class="container">
+        <p class="text-bold">Profil Program Studi detil dapat di akses di <a href="http://polimarin.ac.id" target="_blank" rel="noopener noreferrer">http://polimarin.ac.id</a></p>
     </div>
 </div>
 @endsection
