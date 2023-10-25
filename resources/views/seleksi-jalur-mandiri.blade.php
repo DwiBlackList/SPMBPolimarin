9bdcd284935f4af68c2e6d6e289f820afeaaeb80
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-10-25 10:50:30 +0700

    halaman seleksi jalur remake

--- resources/views/seleksi-jalur-mandiri.blade.php
+++ resources/views/seleksi-jalur-mandiri.blade.php
@@ -2,33 +2,40 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>
-                Seleksi Jalur Mandiri (SJM)
-            </h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>Seleksi Jalur Mandiri</h1>
+    <p>Seleksi Jalur Mandiri merupakan jalur seleksi melalui tes akademik bagi calon mahasiswa baru yang dapat diikuti oleh lulusan SMA/SMK/MA tahun 2021, 2022, atau 2023.</p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
         <div class="row justify-content-center">
-            <!-- Kartu Navigasi kiri -->
-            @include('layouts.navcard')
-            <!-- Konten Kanan -->
-            <div class="col-md-8 col-12">
-                <h4 class="text-bold">SELEKSI JALUR Mandiri (SJM)</h4>
-                <p>
-                    Seleksi Jalur Prestasi merupakan jalur seleksi tanpa tes tertulis bagi calon mahasiswa baru melalui bidang akademik atau minat bakat tertentu. Seleksi berdasarkan pencapaian prestasi akademik (nilai rapor) semester 1 sampai dengan 5. Selain itu jalur ini juga ditujukan bagi calon mahasiswa yang memiliki prestasi (pernah menjadi juara atau finalis di berbagai lomba di tingkat nasional atau provinsi atau kabupaten dalam kurun waktu 3 tahun terakhir) di berbagai bidang seperti olahraga, seni, budaya, dan penalaran (misal olimpiade, karya ilmiah, debat, dan lain-lain).
-                </p>
-                <h4 class="text-bold">SELEKSI JALUR PRESTASI (SJP)</h4>
-                <ol>
-                    <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
-                    <li>aaaSiswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
-                </ol>
+            <div class="col-12 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <p class="h3 text-bold">PERSYARATAN PESERTA</p>
+                        <ol>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
+
+            <div class="col-12 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <p class="h3 text-bold">PERSYARATAN PESERTA</p>
+                        <ol>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
--- resources/views/seleksi-jalur-tes.blade.php
+++ resources/views/seleksi-jalur-tes.blade.php
@@ -2,32 +2,40 @@
 
 @section('content')
 <!-- Paling atas -->
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="bg-warning text-white">
-            <h1>
-                Seleksi Jalur Tes (SJT)
-            </h1>
-        </div>
-    </div>
+<div class="p-5 bg-primary text-white">
+    <h1>Seleksi Jalur Tes</h1>
+    <p>Seleksi Jalur Tes (SJT) merupakan jalur seleksi melalui tes akademik bagi calon mahasiswa baru yang dapat diikuti oleh lulusan SMA/SMK/MA tahun 2021, 2022, atau 2023.</p>
 </div>
 
 <div class="py-5 bg-info"> <!-- Rubah bg dan padding atas -->
     <div class="container">
         <div class="row justify-content-center">
-            <!-- Kartu Navigasi kiri -->
-            @include('layouts.navcard')
-            <!-- Konten Kanan -->
-            <div class="col-md-8 col-12">
-                <h4 class="text-bold">SELEKSI JALUR Tes (SJT)</h4>
-                <p>
-                    Seleksi Jalur Tes (SJT) merupakan jalur seleksi melalui tes akademik bagi calon mahasiswa baru yang dapat diikuti oleh lulusan SMA/SMK/MA tahun 2021, 2022, atau 2023.
-                </p>
-                <ol>
-                    <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
-                    <li>aaaSiswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
-                </ol>
+            <div class="col-12 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <p class="h3 text-bold">PERSYARATAN PESERTA</p>
+                        <ol>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
+
+            <div class="col-12 mb-5">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <p class="h3 text-bold">PERSYARATAN PESERTA</p>
+                        <ol>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                            <li>Siswa tahun terakhir pada pendidikan menengah yang akan lulus pada tahun 2023;</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
