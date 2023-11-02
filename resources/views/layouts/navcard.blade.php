b674f81ad21da0074289dba55f14790fd5cf00f4
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-11-02 11:54:27 +0700

    penghapusan arah  panah

--- resources/views/layouts/navcard.blade.php
+++ resources/views/layouts/navcard.blade.php
@@ -2,97 +2,73 @@
     <div class="card-body">
         <a id="navcard" href="/">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Home
+                 Home
             </div>
         </a>
         <hr>
         <a id="navcard" href="https://akademik.polimarin.ac.id/pmb/" target="_blank">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Portal SPMB
+                 Portal SPMB
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('jadwal-pendaftaran') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Jadwal Pendaftaran
+                 Jadwal Pendaftaran
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('persyaratan') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Persyaratan
+                 Persyaratan
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('alur-pendaftaran') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Alur Pendaftaran
+                 Alur Pendaftaran
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('seleksi-jalur-prestasi') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Seleksi Jalur Prestasi
+                 Seleksi Jalur Prestasi
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('seleksi-jalur-tes') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Seleksi Jalur Tes
+                 Seleksi Jalur Tes
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('seleksi-jalur-mandiri') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Seleksi Jalur Mandiri
+                 Seleksi Jalur Mandiri
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('tes-kekhususan') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Tes Kekhususan
+                 Tes Kekhususan
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('biaya') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Biaya
+                 Biaya
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('kontak') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Kontak
+                 Kontak
             </div>
         </a>
         <hr>
         <a id="navcard" href="{{ route('download') }}">
             <div class="d-flex align-content-center">
-                <span class="material-symbols-outlined">
-                    arrow_forward_ios
-                </span> Download
+                 Download
             </div>
         </a>
     </div>
