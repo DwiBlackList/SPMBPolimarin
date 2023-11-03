a8a5e371bc3068ac4424488c5747c327595d6c87
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-11-03 09:04:50 +0700

    pemindahan file home saat login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,9 @@
                     <ul class="navbar-nav me-auto">
 
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        @guest
                         <li class="nav-item">
                             <a class="nav-link" href="/">Home</a>
                         </li>
@@ -70,7 +70,7 @@
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('tes-kekhususan') }}">Tes Kekhususan</a>
-                                </li>                                
+                                </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('kontak') }}">Kontak</a>
                                 </li>
@@ -94,39 +94,38 @@
                                 </li>
                             </ul>
                         </li>
-
-                        <!-- Authentication Links -->
-                        <!-- @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Jadwal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pengumuman</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Timeline</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">DayaTampung</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Download</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest -->
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
