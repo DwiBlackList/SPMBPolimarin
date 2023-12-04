be01b67484367378bf7f84e01b3338ea8daa08ac
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-12-04 09:11:01 +0700

    css ilang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@ $settings = \App\Models\Settings::findOrFail(1);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
